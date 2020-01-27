f15b47209f2b89aa8d2d25ad108f0a317d14f406
Alicja <alicja@MacBook-Pro-5762.local>
2020-01-27 13:43:08 +0000

    Implemented more features

--- cypress/integration/Footify.feature
+++ cypress/integration/Footify.feature
@@ -7,18 +7,22 @@ Feature: The Footify marketing site
     Then I can see the Footify logo
     And I can see "Play the beautiful game"
 
+  Scenario: Go to sing in form
+    Given I have loaded Footify page
+    Then I click Request an invite button
+
   Scenario: Fill in sign up form with valid user details
     Given I have loaded Footify sign up page
     When I enter "alicjapycio@gmail.com" into the "EMAIL" field
     And I enter "Alicja" into the "FNAME" field
     And I select "11 a-side" from "Type of team you play for" question
     And I select "Organised league" from "Competition" question
-    When I click "Request Invate" button
+    When I click "Request Invite" button
     Then I can see "Thank you for subscribing!"
 
+  @focus
   Scenario: Fill in sign up form with invalid user email
     Given I have loaded Footify sign up page
-    When I enter "invalidemail" into the "Email Adress" field
-    When I click "Request Invate" button.
+    When I enter "invalidemail" into the "EMAIL" field
+    When I click "Request Invite" button
     Then I can see "Please enter a valid email address"
-
--- cypress/integration/Footify/Footify.js
+++ cypress/integration/Footify/Footify.js
@@ -10,14 +10,23 @@ Given ('I have loaded Footify page', () => {
 Then ('I can see the Footify logo', () => {
     cy.get('.top-logo-big').should('be.visible')
 })
+
 And ('I can see {string}', (text) => {
     cy.contains(text)
 })
 
+Given ('I have loaded Footify page', () => {
+    cy.visit('http://www.wearefootify.com/')
+})
+
+Then ('I click Request an invite button', () => {
+    cy.get('#viewFormOne').invoke('attr', 'target', '_self').click();
+})
+
+
 Given ('I have loaded Footify sign up page', () => {
     cy.visit('http://www.wearefootify.com/beta/index.htm')
 })
-
 
 When('I enter {string} into the {string} field', (value, inputName) => {
     cy.get(`input[name="${inputName}"]`).type(value)
@@ -31,8 +40,24 @@ And ('I select "Organised league" from "Competition" question', () => {
     cy.get('input[name="MMERGE3"]').first().click()
 })
 
-When ('I click "Request Invate" button', () => {
-    cy.get('#mc-embedded-subscribe').click()
+When ('I click {string} button', (text) => {
+    cy.contains(text).click()
+})
+Then ('I can see {string}', (text) => {
+    cy.contains('text')
+})
+
+
+Given ('I have loaded Footify sign up page', () => {
+    cy.visit('http://www.wearefootify.com/beta/index.htm')
+})
+
+When('I enter {string} into the {string} field', (value, inputName) => {
+    cy.get(`input[name="${inputName}"]`).type(value)
+})
+
+When ('I click "Request Invite" button', (inputValue) => {
+    cy.get(`input[name="${inputValue}"]`).click({force: true})
 })
 Then ('I can see {string}', (text) => {
     cy.contains('text')
